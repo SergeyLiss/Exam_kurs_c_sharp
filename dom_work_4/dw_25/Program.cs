edcfc508907e12b7ee6056de638b5bc3c0bed6e3
agrss <slog18122018@gmail.com>
2022-10-12 00:03:21 +0300

    домашка номер 4, исправлена задача 25

--- dom_work_4/dw_25/Program.cs
+++ dom_work_4/dw_25/Program.cs
@@ -1,17 +1,16 @@
 ﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 int a = new Random().Next(-20,20);
-int b = new Random().Next(1, 5);
+int b = new Random().Next(1, 7);
 
 bool[] IntToBin(int number)
 {
-    bool[] bin = {};
-    bool[] binT = {true};
-    bool[] binF = {false};
+    int sizeBin = Log2Primit(number);
+    bool[] bin = new bool[sizeBin];
 
-    while (number != 0)
+    for(int i = (sizeBin-1); i >= 0; i --)
     {
-        if ((number & 1) == 1) bin += binT;
-        else bin += binF;
+        if ((number & 1) == 1) bin[i] = true;
+        else bin[i] = false;
 
         number >>= 1;
     }
@@ -25,7 +24,7 @@ int PowAB(int a, int b)
    int sizeBinaryB = binaryB.Length;
    int aStepB = a;
 
-   for(int i = 1; i < sizeBinaryB; i ++)
+   for(int i = 0; i < sizeBinaryB; i ++)
    {
       aStepB *= aStepB;
 
@@ -37,6 +36,19 @@ int PowAB(int a, int b)
    }
 
    return aStepB;
+}
+
+int Log2Primit(int numberToLog)
+{
+    int count = 0;
+
+    while (numberToLog != 1)
+    {
+        numberToLog >>= 1;
+        count ++;
+    }
+
+    return count;
 }
 
 
