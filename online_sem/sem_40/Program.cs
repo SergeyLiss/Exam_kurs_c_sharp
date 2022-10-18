c419efb29e29fb53509157a7713bf8b81d6650a8
SergeyR <slog18122018@gmail.com>
2022-10-18 20:27:22 +0300

    Добавлены 2 задачи по семинару

---
+++ online_sem/sem_40/Program.cs
@@ -0,0 +1,34 @@
+﻿//Существование треугольников
+int[] a = new int[3];
+
+for(int i = 0; i < 3; i ++)
+{
+    a[i] = new Random().Next(1, 10);
+}
+
+//Проверка треугольника
+void Triangle(int[] t)
+{
+    bool vers = true;
+
+    for(int i = 0; i < 3; i ++)
+    {
+        if(t[i] > (t[(i+1)%3] + t[(i+1)%3]))
+        {
+            vers = false;
+            break;
+        }
+    }
+
+    Console.Write("Треугольник со сторонами: " + t[0] + ", " + t[1] + ", " + t[2]);
+    if (vers == true)
+    {
+        Console.WriteLine(" может существовать.");
+    }
+    else
+    {
+        Console.WriteLine(" не может существовать.");
+    }
+}
+
+Triangle(a);
---
+++ online_sem/sem_42/Program.cs
@@ -0,0 +1,22 @@
+﻿//Число в двоичное исчисление
+
+int a = new Random().Next(1, 1000000);
+
+string AtoBin(int a)
+{
+    string c = "";
+    string b = Convert.ToString((a & 1));
+
+    if (a > 0)
+    {
+        c = AtoBin((a >> 1)) + b;
+        return c;
+    } 
+    else
+    {
+        return "0b";
+    }
+}
+
+
+Console.WriteLine(a + "\t" + AtoBin(a) + "\n\t0b" + Convert.ToString(a, 2));
