f687c5101763a0394e4d1da8a22c44c1e4ca97dd
SergeyR <slog18122018@gmail.com>
2022-10-18 21:16:27 +0300

    Добавлены еще 2 задачи по семинару

---
+++ online_sem/sem_44/Program.cs
@@ -0,0 +1,33 @@
+﻿//Числа Фибоначи
+int a = -1;
+do
+{
+    Console.Clear();
+    Console.Write("Введите число больше нуля: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+while (a < 1);
+
+int[] FibNumber(int size)
+{
+    int[] fibonachi = new int[size];
+    fibonachi[0] = 0;
+    fibonachi[1] = 1;
+
+    for(int i = 2; i < size; i ++)
+        fibonachi[i] = fibonachi[i-1] + fibonachi[i-2];
+    
+    return fibonachi;
+}
+
+void PrintMassiv(int[] massiv)
+{
+    int sizeMassiv = massiv.Length;
+    Console.WriteLine("Массив состоит из " + sizeMassiv + " элементов:");
+
+    foreach(double i in massiv) Console.Write(i + "\t");
+
+    Console.WriteLine("");
+}
+
+PrintMassiv(FibNumber(a));
---
+++ online_sem/sem_45/Program.cs
@@ -0,0 +1,31 @@
+﻿//Поэлементное копирование массива
+int[] MassivGeneration(int masMin = 10, int masMax = 1000, int masSize = 0, int masLevel = 1)
+{
+    if(masSize == 0) masSize = new Random().Next(10, 20);
+
+    if(masLevel > 1)
+    {
+        int[] massiv = new int[][masSize];
+        for(int i = 0; i < masLevel; i ++)
+            massiv[i] = MassivGeneration(masMin, masMax, masSize, (masLevel-1));
+    }
+    else
+    {
+        int[] massiv = new int[masSize];
+        for(int i = 0; i < masSize; i ++)
+            massiv[i] = new Random().Next(masMin, masMax);
+    }
+    return massiv;
+}
+
+void PrintMassiv(int[] massiv, int masLevel=1)
+{
+    int sizeMassiv = massiv.Length;
+    Console.WriteLine("Массив состоит из " + sizeMassiv + " элементов:");
+
+    foreach(double i in massiv) Console.Write(i + "\t");
+
+    Console.WriteLine("");
+}
+
+PrintMassiv(MassivGeneration(10,1000,10,2));
