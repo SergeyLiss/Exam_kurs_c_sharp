1ff51eb274922ebc52ad9975d748ed2d6e509ba2
SergeyR <slog18122018@gmail.com>
2022-10-18 22:07:35 +0300

    Добавлена домашка к 6 уроку - dom_work_6 (dw_41 dw_43)

---
+++ dom_work_6/dw_41/Program.cs
@@ -0,0 +1,44 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int a = 0;
+while (a < 1)
+{
+    Console.Clear();
+    Console.Write("Введите число больше нуля: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] MassivGeneration(int masSize = 0, int masMin = -100, int masMax = 100)
+{
+    if(masSize == 0) masSize = new Random().Next(10, 100);
+
+    int[] massiv = new int[masSize];
+
+    for(int i = 0; i < masSize; i ++) massiv[i] = new Random().Next(masMin, masMax);
+
+    return massiv;
+}
+
+void MasElementNull(int[] masN)
+{
+    int count = 0;
+
+    foreach(int i in masN)
+        if(i > 0)
+            count  += 1;
+
+    Console.Write("В массиве из " + masN.Length + " элементов содержится " + count + " элемента(ов) больше нуля.");
+}
+
+void PrintMassiv(int[] massiv)
+{
+    int sizeMassiv = massiv.Length;
+    Console.WriteLine("Массив состоит из " + sizeMassiv + " элементов:");
+
+    foreach(int i in massiv) Console.Write(i + "\t");
+
+    Console.WriteLine("");
+}
+
+int[] b = MassivGeneration(a);
+PrintMassiv(b);
+MasElementNull(b);
---
+++ dom_work_6/dw_43/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] ReadTwoLines()
+{
+    Console.Clear();
+    double[] lines = new double[4];
+    int sdvig = 0;
+    string[] print = {"Первая прямая, заданная уравнением y = k1 * x + b1 .", "k1", "b1",
+                        "Вторая прямая, заданная уравнением y = k2 * x + b2", "k2", "b2"};
+
+    for(int i = 0; i < print.Length; i ++)
+    {
+        if((i%3) == 0)
+        {
+            Console.WriteLine(print[i]);
+            sdvig ++;
+        }
+        else
+        {
+            Console.Write("Введите значение " + print[i] + ": ");
+            lines[i-sdvig] = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+    return lines;
+}
+
+void IntersectionLines(double[] lines)
+{
+    if((lines[0]-lines[2]) == 0)
+        Console.WriteLine("Заданный прямые - ПАРАЛЛЕЛЬНЫ.");
+    else
+    {
+        double x = (lines[3]-lines[1]) / (lines[0]-lines[2]);
+        double y = x * lines[0] + lines[1];
+
+        Console.WriteLine("Точка пересечения заданных прямых: (" + x + "; " + y + ").");
+    }
+}
+
+IntersectionLines(ReadTwoLines());
+
+
