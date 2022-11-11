95bbbfc89ef69c4c134fcfc12ae7f5377a933761
SergeyR <slog18122018@gmail.com>
2022-11-11 22:51:07 +0300

    Добавлена домашка dom_work dw_64 задача 64

---
+++ dom_work_9/dw_64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void InConsole()
+{
+    Console.Clear();
+    Console.WriteLine("Будут выведены натуральные числа в диапозоне от N до 1.");
+    Console.Write("Введите число N, больше единицы: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    
+    if(n > 1){Console.WriteLine(NumberDec(n));}
+    else {Console.WriteLine("N должно быть больше единицы.");}
+
+    
+}
+
+string NumberDec(int number)
+{
+    if(number != 1) {return (Convert.ToString(number) + ", " + NumberDec(number-1));}
+    else {return "1.";}
+}
+
+InConsole();
